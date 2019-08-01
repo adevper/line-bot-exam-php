b4ffce4ffbd347dbc9794f86b65694e953ac7222
Adevper <nikomkumsom@gmail.com>
2019-08-01 15:54:50 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -8,7 +8,7 @@ $access_token = 'dbT/YlbWgSdq2HTmh+qvKUz19cfMHHiFjO6no7px9vt60Dtn1CAXcSXghZxxBbN
 
 $channelSecret = '1e8a299be61a633d05f20ab026852f3a';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'U2394049070835aa09a3998e1fafeb89b';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
